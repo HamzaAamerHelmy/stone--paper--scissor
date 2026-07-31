3521f105b471b7e1d47fa52095b724516a63980d
Hamza Aamer <Hamza.Aamer@outlook.com>
2026-07-31 15:47:31 +0300

    Fix formatting of game over screen header for improved readability

--- main.cpp
+++ main.cpp
@@ -227,7 +227,7 @@ void playWinSound()
 void printGameOver(int numberOfRounds, enChoices choiceOfPlayer, enChoices choiceOfComputer)
 {
     string finalWinner = "";
-    cout << "                   ___________________  ____________________________________________________________________________" << endl
+    cout << "                   _______________________________________________________________________________________________" << endl
          << endl;
     cout << "                                                 +++     G a m e O v e r     +++" << endl;
     cout << "                   _______________________________________________________________________________________________" << endl
